c4668fcbe148c66ded08cf211f2f1749e91e5a5a
duxiaofeng-github <csdxf297951292@gmail.com>
2023-01-18 17:20:55 +0800

    finish setup page

--- plugin/popup/Setup.tsx
+++ plugin/popup/Setup.tsx
@@ -1,22 +1,51 @@
 import { useRexContext } from "@jimengio/rex";
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { paths } from "./routes";
-import { IStore } from "./store";
+import { globalStore, IStore } from "./store";
 import "./setup.css";
-import { Input, Typography } from "antd";
+import { Button, Form, Input, Typography } from "antd";
+import { useWallet } from "./useWallet";
 
 export const Setup: React.FunctionComponent = () => {
-  const { privateKey } = useRexContext((store: IStore) => store);
+  const wallet = useWallet();
 
-  if (privateKey) {
+  if (wallet) {
     return <Navigate to={paths.home} />;
+  }
+
+  function submit(privateKey: string) {
+    globalStore.update((store) => {
+      store.privateKey = privateKey;
+    });
   }
 
   return (
     <div className="setup-container">
-      <Typography.Title>Wallet Setup</Typography.Title>
-      <Input />
+      <Typography.Title level={2} className="title">
+        Wallet Setup
+      </Typography.Title>
+      <Typography.Paragraph type="secondary" className="desc">
+        You need to enter your wallet private key to enable this plugin
+      </Typography.Paragraph>
+      <Form
+        onFinish={({ privateKey }) => {
+          submit(privateKey);
+        }}
+      >
+        <Form.Item
+          name="privateKey"
+          rules={[
+            // we should do a validator here to check if the privateKey is valid later
+            { required: true, message: "Private key is required" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Button block type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
---
+++ plugin/popup/useWallet.ts
@@ -0,0 +1,22 @@
+import { useRexContext } from "@jimengio/rex";
+import { ethers } from "ethers";
+import { useMemo } from "react";
+import { IStore } from "./store";
+
+export function useWallet() {
+  const { privateKey } = useRexContext((store: IStore) => store);
+  const wallet = useMemo(() => {
+    if (!privateKey) {
+      return undefined;
+    }
+
+    const rpcProvider = new ethers.providers.JsonRpcProvider(
+      "https://rpc.ankr.com/polygon_mumbai",
+      80001
+    );
+
+    return new ethers.Wallet(privateKey, rpcProvider);
+  }, [privateKey]);
+
+  return wallet;
+}
